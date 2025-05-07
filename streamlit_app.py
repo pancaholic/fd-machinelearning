4f268a0cbe74efff69f40145586e598351df2419
Putra Panca <145687657+pancaholic@users.noreply.github.com>
2025-05-07 09:06:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Comparative Model For Fraud Detection')
 
-st.write('Comparative study for a machine learning model to decide which is the best for a certain dataset')
+st.info('Comparative study for a machine learning model to decide which is the best for a certain dataset')
+
+df = df.read_csv('https://raw.githubusercontent.com/pancaholic/skripsi/refs/heads/master/Finding/Fraud%20Detection%20Dataset.csv?token=GHSAT0AAAAAADCYTOUNIRTCV6SOTDE34HCG2A2YAEQ')
+
+df
+
+
